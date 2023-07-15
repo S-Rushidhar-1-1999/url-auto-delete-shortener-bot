7f21c8327dfc94f26444889d377e0d719aed2555
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2023-07-15 20:29:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -406,7 +406,7 @@ async def shorten_url(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://gplinks.in/api'
+    url = f'https://atglinks.com/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
